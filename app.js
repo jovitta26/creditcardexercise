43cd4a65f70583eeb77c8feaa3cf9f9c823d464f
johana <jovitta26@gmail.com>
2017-10-05 15:40:37 -0300

    ejercicio2

---
+++ app.js
@@ -0,0 +1,21 @@
+function creditCard() {
+
+      var creditCard = prompt ("Ingrese numero de tarjeta")
+
+             for ( var i= 0 ; i >=creditCard.length; i++)
+                 if ( object[i].length > num)
+              return creditCard;
+}
+
+           if (creditCard.length > 19)
+      console.log("")
+
+   function invertir (str) {
+     var x = str.length;
+     var stringInvertida="";
+
+     while (x>=0) {
+      stringInvertida = stringInvertida + string.charAt(x);
+       x--
+       return stringInvertida
+     }
